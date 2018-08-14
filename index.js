31a4c388651d2d658d83ba29b7ad0dc114a8a376
Tomflynn Beltsazar <tomflynnbeltsazar@gmail.com>
2018-08-14 14:00:30 +0700

    Fixed dead waifu (6th attempt)

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Discord = require("discord.js");
+const { Discord, Attachment } = require("discord.js");
 const client = new Discord.Client();
 const YTDL = require("ytdl-core");
 const Kaori = require('kaori');
