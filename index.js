daf7dc9c08d3308b0f2e93f2078705793898d4a4
beltsazars <tomflynnbeltsazar@gmail.com>
2019-01-28 13:49:57 +0700

    Imported new libs

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const request = require("request");
 const YTDL = require("ytdl-core");
 const Kaori = require('kaori');
 const kaori = new Kaori();
