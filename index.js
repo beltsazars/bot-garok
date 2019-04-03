4460e209a7fa3fc899e63552812685660d7da1b6
Lite <ucihaibna@yahoo.com>
2019-04-03 15:26:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ client.on("message", async message => {
                                     fs.mkdirSync(dir);
                                 }
                                 const file = fs.createWriteStream(dir+"/"+mapSet+".zip");
-                                fs.createReadStream(mapSet+".zip").pipe(unzipper.Extract({ path: dir+"/"+mapSet }));
+                                fs.createReadStream(dir+"/"+mapSet+".zip").pipe(unzipper.Extract({ path: dir+"/"+mapSet }));
                                 //console.log(getFiles("mapSet/"));
                             const dispatcher = connection.playFile(dir+"/"+mapSet+"/audio.mp3");
                             }).catch(e => {
